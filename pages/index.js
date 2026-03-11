375c57ae8004061d2cd4725c635e9a35076f041f
cardinaliii <pedrocardinalidev@gmail.com>
2026-03-11 16:41:59 +0000

    new msg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+js
 function Home() {
-   return <h1>INTER VAMOS TE MATAR</h1>
+   return <h1>Te amo mãe! ❤️</h1> 
 }
 
 export default Home;
+
+<body>
+    <script src="https://open.spotify.com/embed/iframe-api/v1" async></script> 
+
+    <div id="embed-iframe">
+        window.onSpotifyIframeApiReady = (IFrameAPI) => 
+};
+
+
+<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/4yBiZFLXn0n9AUJ5sWO88k?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+
+     </body>
+
+     
